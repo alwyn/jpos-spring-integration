b345763d59517fdc7905f3654f4ab4e437071fc3
Alwyn Schoeman <alwyn.schoeman@olspayments.com>
2018-08-29 12:26:00 -0400

    Modified group id to tech.nomads

---
+++ src/main/kotlin/tech/nomads/jpos/spring/integration/serializer/IsoCodecs.kt
@@ -0,0 +1,10 @@
+package tech.nomads.jpos.spring.integration.serializer
+
+class IsoCodecs {
+
+    companion object Factory {
+        fun Xml2003ChannelSerializer() =
+            Xml2003ChannelSerializer
+    }
+
+}
